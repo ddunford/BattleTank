465970b99544e54989cc0b75be372ae8be8af9c4
Dan Dunford <me@dandunford.info>
2018-03-18 16:31:31 +0000

    Have AI controller get the pawn tank instance

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -15,11 +15,12 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public: 
+private:
 	ATank* GetControlledTank() const; 
 	
 	void BeginPlay() override; 
 
+	ATank* GetPlayerTank() const; 
+
 	ATank* ControlledTank = nullptr;
-	
 };

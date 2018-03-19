2b6e79f7613a2cf078b6a421fd38af50c86fd29d
Dan Dunford <me@dandunford.info>
2018-03-19 17:25:25 +0000

    Refactoring. Class forwarders. Clamping elevate.

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,10 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "TankBarrel.h"
+#include "BattleTank.h"
+#include "Math/UnrealMathUtility.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amoutn this frame 
 	// Given a max elevation speed, and the frame time
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+
+	auto Clamp = FMath::Clamp(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(
+		FRotator(
+			Clamp,
+			0, 
+			0
+		)
+	);
 
 }
--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -2,12 +2,14 @@
 
 #pragma once
 
-#include "TankAimingComponent.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h" //Put new includes above this line 
 
+//forward declarations 
 class UTankBarrel;
+class UTankAimingComponent;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn

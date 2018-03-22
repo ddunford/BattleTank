d72abe4beb6273c20a54127b38f24f54f36f42c8
Dan Dunford <me@dandunford.info>
2018-03-22 16:06:39 +0000

    Add firing state enum

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+//Enum for aiming state 
+UENUM()
+enum class EFiringStatus : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 // Foward declaration
 class UTankBarrel;
 class UTankTurret;
@@ -28,6 +37,9 @@ public:
 private: 
 	UTankBarrel* Barrel = nullptr;
 	UTankTurret* Turret = nullptr;
-		
+
+	EFiringStatus FiringStatus = EFiringStatus::Reloading;
+
 	void MoveBarrelTowards(FVector AimDirection);
+
 };
